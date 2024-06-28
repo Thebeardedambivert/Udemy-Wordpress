30e6cdab08b1ddb49eb183c1e2d3c7ace4b7499c
Bearded ambivert <beardedceey97@gmail.com>
2024-06-28 05:59:44 +0100

    changes in the plugin

---
+++ wp-content/plugins/udemy-plus/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-blocks'), 'version' => '6b76cba2ba413f6d26e0');
---
+++ wp-content/plugins/udemy-plus/includes/register-blocks.php
@@ -0,0 +1,6 @@
+<?php
+
+function up_register_blocks()
+{
+    UP_PLUGIN_DIR . "build/block.json";
+}
