cff783c6747ff54743578f78f3fb95fa6504f987
Bearded ambivert <beardedceey97@gmail.com>
2024-06-29 07:08:23 +0100

    updated page header

--- includes/blocks/page-header.php
+++ includes/blocks/page-header.php
@@ -1,6 +1,25 @@
 <?php
 
-function up_page_header_render_cb()
+function up_page_header_render_cb($atts)
 {
+    $heading = esc_html($atts['content']);
 
+    if ($atts['showCategory']) {
+        $heading = get_the_archive_title();
+    }
+
+    ob_start();
+    ?>
+
+    <div className="wp-block-udemy-plus-page-header">
+        <div className="inner-page-header">
+            <h1><?php echo $heading; ?></h1>
+        </div>
+    </div>
+    <?php
+
+    $output = ob_get_contents();
+    ob_end_clean();
+
+    return $output;
 }
